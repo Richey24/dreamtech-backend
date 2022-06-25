9eabaac9535840e2b18a3635a56542a40c73a46d
Viktoh-Ikenna <viktoh675@gmail.com>
2022-06-25 03:52:48 +0100

    My first commit

--- controller/userController/createUser.js
+++ controller/userController/createUser.js
@@ -1,18 +1,24 @@
-const { User } = require('../../schema')
+const { User } = require("../../schema");
 
 const createUserController = async (req, res) => {
-    const { firstname, lastname, email, gender,phone } = req.body
-    const check = await User.findOne({ email })
-    if (check) {
-        res.status(201).json({ message: "user with this email already exists" })
-        return
+  const { firstname, lastname, email, gender, phone } = req.body;
+  const check = await User.findOne({ email });
+  if (check) {
+    res.status(201).json({ message: "user with this email already exists" });
+    return;
+  }
+  if (firstname) {
+    try {
+      const user = await User.create(req.body);
+      res.status(200).json({ status: true, user });
+    } catch (err) {
+      res.status(200).json({ status: false, err });
     }
-    if (firstname && lastname && gender && phone) {
-        const user = await User.create(req.body)
-        res.status(200).json({ status:true, user })
-    } else {
-        res.status(203).json({ message: "please send all the required information" })
-    }
-}
+  } else {
+    res
+      .status(203)
+      .json({ message: "please send all the required information" });
+  }
+};
 
-module.exports = createUserController
+module.exports = createUserController;
