a4725c6813e8a9e80a2653472844dc6fdd00c857
Rejoice <uahomorejoice@gmail.com>
2022-08-03 02:47:43 +0100

    changes

--- controller/userController/createUser.js
+++ controller/userController/createUser.js
@@ -2,29 +2,25 @@ const { User } = require("../../schema");
 const argon2 = require("argon2");
 
 const createUserController = async (req, res) => {
-  const { firstname, email, password } = req.body;
-  if (!firstname || !email || !password) return;
+  const { firstname, email, mypassword } = req.body;
+  if (!firstname || !email || !mypassword) {
+    res
+      .status(203)
+      .json({ message: "please send all the required information" });
+    return;
+  }
   const check = await User.findOne({ email });
   if (check) {
     res.status(201).json({ message: "user with this email already exists" });
     return;
   }
-  if (firstname) {
-    try {
-      let hashPass = await argon2.hash(password);
-      req.body.password = hashPass;
-      console.log(req.body);
-      const user = await User.create(req.body);
-      const { password, ...mainUser } = user._doc;
-      res.status(200).json({ status: true, mainUser });
-    } catch (err) {
-      res.status(200).json({ status: false, err });
-    }
-  } else {
-    res
-      .status(203)
-      .json({ message: "please send all the required information" });
-  }
+  let hashPass = await argon2.hash(mypassword);
+  console.log(req.body);
+  req.body.password = hashPass;
+  console.log(req.body);
+  const user = await User.create(req.body);
+  const { password, ...mainUser } = user._doc;
+  res.status(200).json({ status: true, mainUser });
 };
 
 module.exports = createUserController;
